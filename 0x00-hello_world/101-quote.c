75bd325a13b31d4ff231190baadb4ebda5ae9acd
Bomberkill <ronaldkamgaing4@gmail.com>
2022-12-09 12:13:42 +0100

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n");
 	return(1);
 
 }
