7f9d1dbfd581c9134204187f1f0e9413e0af1380
Bomberkill <ronaldkamgaing4@gmail.com>
2022-12-12 12:20:05 +0100

    comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n, m;
+
+	for (n = '0'; n < '9'; n++)
+	{
+		for (m = n + 1; m <= '9'; m++)
+		{
+			putchar(d);
+			putchar(p);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
