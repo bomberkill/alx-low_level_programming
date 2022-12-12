7735347609edfdba9176b7f62d0892edb7e00e7d
Bomberkill <ronaldkamgaing4@gmail.com>
2022-12-12 13:05:53 +0100

    comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a,z,s;
+	for (a = '0'; a < '8'; a++)
+	{
+		for (z = a + 1; z < '9'; z++)
+		{
+			for (s = z + 1; s <= '9' ; s++)
+			{
+
+				putchar(a);
+				putchar(z);
+				putchar(s);
+
+				if (a == '7' && z == '8')
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
