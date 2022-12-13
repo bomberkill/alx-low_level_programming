500ec656310130449de4db4dcad48896ad1a3336
Bomberkill <ronaldkamgaing4@gmail.com>
2022-12-13 15:24:21 +0100

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -14,6 +14,13 @@ int _putchar(char c)
 }
 int main (void)
 {
-	printf(_putchar('r'),_putchar('a'), _putchar('h'), _putchar('c'), _putchar('t'), _putchar('u'), _putchar('p'), _putchar('_'));
+	_putchar('_');
+        _putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
 	return(0);
 }
