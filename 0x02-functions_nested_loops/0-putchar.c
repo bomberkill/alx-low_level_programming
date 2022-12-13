0b42d35189e7969f2c5ad1a13fb45ec5ae3331be
Bomberkill <ronaldkamgaing4@gmail.com>
2022-12-13 16:05:01 +0100

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,14 +3,15 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * _putchar - writes the character mot to stdout
+ * @mot: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int main(void) /* fonction main */
+int main(void) 
 {
+/*return 0 (success)*/
 	char mot[8] = "_putchar";
 	int i;
 
